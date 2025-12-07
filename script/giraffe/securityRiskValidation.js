666cab2c37416746ac5baddca8c411a281885bb0
iiifox <86429225+iiifox@users.noreply.github.com>
2025-12-07 14:21:14 +0800

    Update securityRiskValidation.js

--- script/giraffe/securityRiskValidation.js
+++ script/giraffe/securityRiskValidation.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         长颈鹿过安全风险验证
-// @namespace    https://luei.me/
+// @namespace    https://calm.luei.me/
 // @version      1.0.0
 // @description  自动判断捕获、风险替换。传码（目前只支持小刀系）、qb破风险（避免自付暂时没写）
 // @author       luei
@@ -9,8 +9,8 @@
 // @grant        GM_getValue
 // @grant        GM_xmlhttpRequest
 // @run-at       document-start
-// @updateURL    https://luei.me/script/giraffe/securityRiskValidation.js
-// @downloadURL  https://luei.me/script/giraffe/securityRiskValidation.js
+// @updateURL    https://calm.luei.me/script/giraffe/securityRiskValidation.js
+// @downloadURL  https://calm.luei.me/script/giraffe/securityRiskValidation.js
 // @connect      081w5a8cim.top
 // @connect      8w0m6rjg3l.top
 // ==/UserScript==

42ef56473da4c29a1461f891aed552ac3d297c6f
iiifox <86429225+iiifox@users.noreply.github.com>
2026-02-25 11:16:44 +0800

    Update and rename smartTasksaaa.js to smartTasks.js

---
+++ assets/script/xy/smartTasks.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         星悦智能任务
+// @namespace    https://calm.luei.me/
+// @version      2.0.0
+// @description  定时执行自动任务，同时遇到出码失败的账号自动转为充值中
+// @author       iiifox
+// @match        *://sdk.wy7l9.com/*
+// @run-at       document-end
+// @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_xmlhttpRequest
+// @connect      sdk.wy7l9.com
+// @updateURL    https://luei.me/assets/script/xy/smartTasks.js
+// @downloadURL  https://luei.me/assets/script/xy/smartTasks.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    
+})();
